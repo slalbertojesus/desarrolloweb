ed35bc2289cbb38181f62c6bde09000e532e8fcd
Alberto de Jesús Sánchez López <slalbertojesus@gmail.com>
2020-04-29 21:45:31 -0500

    Se añadió caso de uso iniciar sesión

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 
 from .views import (
-	display_singin,
+	display_home,
+	display_login,
 	display_register,
+	logout_user,
 )
 
 app_name = 'accounts'
 
 urlpatterns = [
-	path('', display_singin, name="homepage"),
+	path('login/', display_login, name="login"),
+	path('logout/', logout_user, name="logout"),
 	path('register/', display_register, name="register"),
+	path('', display_home, name="home"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,43 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm  
+from django.contrib import messages
+
 from .forms import CreateUserForm 
 
+def display_home(request):
+    context = {}
+    return render(request, 'accounts/home.html', context)
+
 def display_register(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
                 form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Cuenta creada para' + user)
+                return redirect('/login')
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
-def display_singin(request):
+def display_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+        if user is not None: 
+            login(request, user)
+            return redirect('/')
+        else: 
+            messages.info(request, 'Usuario o contraseña es incorrecta')
     context = {}
-    return render(request, 'accounts/home.html', context)
+    return render(request, 'accounts/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login')
 
